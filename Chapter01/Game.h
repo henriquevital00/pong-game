ebdf28219a274992b6d5555f6c9fc4eb7c5bec29
GabrielBueno200 <gabrielbueno200@gmail.com>
2022-03-04 12:53:39 -0300

    added more than one ball (balls list) and player score board

---
+++ Chapter01/Game.h
@@ -0,0 +1,70 @@
+// ----------------------------------------------------------------
+// From Game Programming in C++ by Sanjay Madhav
+// Copyright (C) 2017 Sanjay Madhav. All rights reserved.
+// 
+// Released under the BSD License
+// See LICENSE in root directory for full details.
+// ----------------------------------------------------------------
+
+#pragma once
+#include "SDL/SDL.h"
+#include <list>
+#include <iostream>
+#include <sstream>
+#include <string>
+
+using namespace std;
+
+// Vector2 struct just stores x/y coordinates
+// (for now)
+struct Vector2
+{
+	float x;
+	float y;
+};
+
+typedef struct {
+	// Position of ball
+	Vector2 position;
+	// Velocity of ball
+	Vector2 velocity;
+} Ball;
+
+// Game class
+class Game
+{
+public:
+	Game();
+	// Initialize the game
+	bool Initialize();
+	// Runs the game loop until the game is over
+	void RunLoop();
+	// Shutdown the game
+	void Shutdown();
+private:
+	// Helper functions for the game loop
+	void ProcessInput();
+	void UpdateGame();
+	void GenerateOutput();
+
+	// Window created by SDL
+	SDL_Window* mWindow;
+	// Renderer for 2D drawing
+	SDL_Renderer* mRenderer;
+	// Number of ticks since start of game
+	Uint32 mTicksCount;
+	// Game should continue to run
+	bool mIsRunning;
+
+	
+	// Pong specific
+	// Direction of paddle
+	int mPaddleDir;
+	// Position of paddle
+	Vector2 mPaddlePos;
+	
+	// Balls list
+	list<Ball> mBalls;
+
+	int playerBoard;
+};
